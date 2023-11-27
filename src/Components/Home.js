6d8cee001b7e11f632fc5e783e199055cd258cfd
Matthew Luk <matthew.luk@yahoo.com>
2023-11-26 20:34:49 -0800

    added onclick handler

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import '../App.css';
-import { generatePassword } from './Functions';
-import { generate, count } from "random-words";
+import { generatePassword, generateApplePassword } from './Functions';
 
 const Home = () => {
     const [password, setPassword] = useState("")
@@ -10,19 +9,24 @@ const Home = () => {
     const [special,setSpecial] = useState(false)
     const [number,setNumber] = useState(false)
 
-    const submitHandler = (e) => {
+    const passwordHandler = (e) => {
         e.preventDefault()
-        if(e.target[0].value <= 4){
-            setError("Please pick a number greater than 4")
+        if(e.target.form[0].value < 4){
+            setError("Please pick a number greater than 3")
             setPassword("")
-        }else if(e.target[0].value > 32){
+        }else if(e.target.form[0].value > 32){
             setError("Please pick a number less than 33")
             setPassword("")
         }else{
-            console.log(generate({minLength:4,maxLength:4}))
-            setPassword(generatePassword(e.target[0].value, upper, special, number))
+            setPassword(generatePassword(e.target.form[0].value, upper, special, number))
             setError("")
         }
+    }
+
+    const applePasswordHandler = (e) => {
+        e.preventDefault()
+        setPassword(generateApplePassword())
+        setError("")
     }
 
     const upperHandler = (e) => {
@@ -39,7 +43,11 @@ const Home = () => {
 
     return (
         <div className='container'>
-            <form onSubmit={submitHandler} className='passwordForm'>
+            <div className='titles'>
+                <p className='title'>Random Password Generator</p>
+                <p className='subtitle'>By Matthew Luk</p>
+            </div>
+            <form className='passwordForm'>
                 <div className='errorDisplayed'>
                     {
                         error?
@@ -49,13 +57,14 @@ const Home = () => {
                 </div>
                 <p>Generated Password: {password}</p>
                 <div className='length'>
-                    <label>Password length (maximum 32 characters):</label>
-                    <input type='number'/>
+                    <label>Password length (between 4 and 32 characters):</label>
+                    <input className={`${error? "errorBorder": "" }`} type='number' placeholder='0'/>
                 </div>
                 <div className='radioButtons'>
                     <p>Password requirements:</p>
                     <div>
                         <input type='checkbox' value='true' onChange={upperHandler}/>
+                        <span className='checkmark'></span>
                         <label>Upper Case?</label>
                     </div>
                     <div>
@@ -67,7 +76,10 @@ const Home = () => {
                         <label>Numbers?</label>
                     </div>
                 </div>
-                <button>Generate Password</button>
+                <div className='buttons'>
+                    <button onClick={passwordHandler} className='btn1'>Generate Password</button>
+                    <button onClick={applePasswordHandler} className='btn2'>Generate Apple Strong Password</button>
+                </div>
             </form>
         </div>
     )
