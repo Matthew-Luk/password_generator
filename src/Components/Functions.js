fe5c69532e7a945e1ec679a3f527e4c21dcdce9c
Matthew Luk <matthew.luk@yahoo.com>
2023-11-26 20:34:29 -0800

    created apple generated password function

--- src/Components/Functions.js
+++ src/Components/Functions.js
@@ -6,6 +6,7 @@ export function generatePassword(int, upper, special, num){
     let str = []
     let lowerCase = "abcdefghijklmnopqrstuvwxyz"
     let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    // eslint-disable-next-line
     let specialCharacters = "!@#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
     let numbers = "0123456789"
     str.push(getRandomCharacter(lowerCase))
@@ -23,6 +24,25 @@ export function generatePassword(int, upper, special, num){
         str.push(getRandomCharacter(lowerCase))
     }
     let shuffledArray = str.sort((a, b) => 0.5 - Math.random());
+    shuffledArray = shuffledArray.join("")
+    return shuffledArray
+}
+
+export function generateApplePassword(){
+    let str = []
+    let lowerCase = "abcdefghijklmnopqrstuvwxyz"
+    let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let numbers = "0123456789"
+    for(let i=0;i<2;i++){
+        str.push(getRandomCharacter(numbers))
+        str.push(getRandomCharacter(upperCase))
+    }
+    for(let i=0;i<14;i++){
+        str.push(getRandomCharacter(lowerCase))
+    }
+    let shuffledArray = str.sort((a, b) => 0.5 - Math.random());
+    shuffledArray.splice(5, 0, "-")
+    shuffledArray.splice(11, 0, "-")
     shuffledArray = shuffledArray.join("")
     return shuffledArray
 }
